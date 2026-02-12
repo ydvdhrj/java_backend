8f4fc0d0132be755074b16249777fcb85c038140
ydvdhrj <ydv.dhrj123@gmail.com>
2026-02-12 11:45:14 +0530

    remove metadata and test using csv test file

---
+++ junit_testing/src/main/java/com/testing/EvenOrOdd.java
@@ -0,0 +1,8 @@
+package com.testing;
+
+public class EvenOrOdd {
+
+	public String evenOrOdd(int a) {
+		return a%2==0?"even":"odd";
+	}
+}
--- junit_testing/src/test/java/com/junit_testing/ProgramTest.java
+++ junit_testing/src/test/java/com/junit_testing/ProgramTest.java
@@ -4,37 +4,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import com.testing.EvenOrOdd;
 import com.testing.Program;
 
 public class ProgramTest {
 	
-	@ParameterizedTest
-	@ValueSource(strings= {"tenet","radar","aba"})
+//	@ParameterizedTest
+//	@ValueSource(strings= {"tenet","radar","aba"})
 	public void isPalindromeTest(String str) {
 		Program p=new Program();
 		assertTrue(p.isPalindrome(str));
 	}
 	
-	@ParameterizedTest
-	@ValueSource(ints= {1,15,23,42})
+//	@ParameterizedTest
+//	@ValueSource(ints= {1,15,23,42})
 	public void numberIsPositiveTest(int a) {
 		Program p = new Program();
 		assertTrue(p.numberIsPositive(a));
 	}
 	
-	@ParameterizedTest
-	@CsvSource({
-		"1,2,3",
-		"5,5,10",
-		"5,3,7"
-	})
+//	@ParameterizedTest
+//	@CsvSource({
+//		"1,2,3",
+//		"5,5,10",
+//		"5,3,7"
+//	})
 	public void addTest(int a,int b, int expectedResult) {
 		
 		Program p = new Program();
 		int actualres = p.add(a, b);
 		assertEquals(expectedResult, actualres);
+	}
+	
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/capgemini.csv",numLinesToSkip = 1)
+	public void evenOrOddTest(String input,String expected) {
+		
+		EvenOrOdd eoo = new EvenOrOdd();
+		String actualres = eoo.evenOrOdd(Integer.parseInt(input));
+		assertEquals(expected,actualres);
 	}
 }
